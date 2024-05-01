f11b13814003d4032435310e07bcc0e2856ca23a
amithuu <amithkulkarni98@gmail.com>
2024-05-01 14:54:38 +0530

    'added database'

--- proj/settings.py
+++ proj/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # 'default':{
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'user-role',
-    #     'USER': 'root',
-    #     'PASSWORD': '',
-    #     'HOST': 'localhost',
-    #     'PORT': '5432'
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'task',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3307'
+    }
 }
 
 
